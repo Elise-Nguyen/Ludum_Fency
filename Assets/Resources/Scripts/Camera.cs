f662331412e4311d862522f79ff120c9dc1913db
Elise_NGUYEN <elise.nguyen@edu.devinci.fr>
2020-10-03 23:30:15 +0200

    Camera suit le joueur en sidescroll

---
+++ Assets/Resources/Scripts/Camera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Vector3 position;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = new Vector3(player.transform.position.x, 0,-5);
+        transform.position = position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        position = new Vector3(player.transform.position.x, 0,-5);
+        transform.position = position;
+    }
+}
--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -41,4 +41,9 @@ public class Player : MonoBehaviour
             crouch = false;
         }
     }
+
+    public void Action()
+    {
+
+    }
 }
