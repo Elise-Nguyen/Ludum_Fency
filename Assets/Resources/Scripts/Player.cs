158375ac150c999751f89ff2bea57c02a77c39c4
Elise_NGUYEN <elise.nguyen@edu.devinci.fr>
2020-10-03 16:42:48 +0200

    Déplacement joueur ajouté
    Pour bouger, flèche directionnel du clavier

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    public Vector2 position;
+    public float speed = 1f;
+    public Rigidbody2D rb;
+    public bool crouch = false;
+        
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.velocity = Vector2.right * speed;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.velocity = Vector2.left * speed;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            crouch = true;
+        }
+        if(Input.GetKeyUp(KeyCode.RightArrow)
+            || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(0,0);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            crouch = false;
+        }
     }
 }
