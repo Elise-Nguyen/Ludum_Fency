165aca5d502e3b22f667d16f6ad5d8908139509a
Dorian Gélas <dorian.gelas@gmail.com>
2020-10-04 14:38:41 +0200

    Debut GameManager

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum PlayerPhase {Baby, Adult, GrandFather, Squeleton}
+    public PlayerPhase currentPhase = PlayerPhase.Baby;
+    public static GameManager instance;
+    
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this; 
+        }
+        else
+        {
+            Debug.LogError(this + " in " + gameObject.name + " has be destroyed because another " + this + " already exists");
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void ChangePlayerState(PlayerPhase selectedPhase)
+    {
+        currentPhase = selectedPhase;
+        switch(selectedPhase){
+            case PlayerPhase.Baby:
+            {
+
+            }
+            break;
+            case PlayerPhase.Adult:
+            {
+
+            }
+            break;
+            case PlayerPhase.GrandFather:
+            {
+
+            }
+            break;
+            case PlayerPhase.Squeleton:
+            {
+
+            }
+            break;
+
+        }
+    }
+
+}
