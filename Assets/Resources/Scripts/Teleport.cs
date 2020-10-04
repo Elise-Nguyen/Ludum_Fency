7b233d61ed7469b1ec91f2b9583b29ae84579bb8
Elise_NGUYEN <elise.nguyen@edu.devinci.fr>
2020-10-04 17:28:36 +0200

    modif ajout sur player et tp

--- Assets/Resources/Scripts/Teleport.cs
+++ Assets/Resources/Scripts/Teleport.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Teleport : MonoBehaviour
 {
    public Transform destinationPosition;
+    /*
    void OnTriggerEnter2D(Collider2D other)
    {
        if(other.tag == "Player")
        {
            TeleportPlayerTo(other.gameObject);
        }   
-   }
+   }*/
 
-   void TeleportPlayerTo(GameObject player)
+   public void TeleportPlayerTo(GameObject player)
    {
+        new WaitForSecondsRealtime(1f);
        player.transform.position = destinationPosition.position;
    }
+
+    public static void MyDelay(int seconds)
+    {
+        TimeSpan ts = DateTime.Now + TimeSpan.FromSeconds(seconds);
+
+        do { } while (DateTime.Now < ts);
+    }
 }
