d683b8e18aaa0f61b456fe4e7f0656f5c3284467
Yekonori <yannisdemacedo@gmail.com>
2020-10-05 20:52:09 +0200

    Baby Enigma & Change Form

--- Assets/Resources/Scripts/Teleport.cs
+++ Assets/Resources/Scripts/Teleport.cs
@@ -19,23 +19,41 @@ public class Teleport : MonoBehaviour
 
     public void TeleportPlayerTo(GameObject player)
     {
+        Player character = player.GetComponent<Player>();
 
-       // if (!waitActive)
-        
-            //Debug.Log("Wait");
-            //StartCoroutine(Wait());
-        
-        player.transform.position = destinationPosition.position;
-        
+        if (Player.currentPhase == PlayerPhase.Baby)
+        {
+            if (!character.babyEnigma)
+            {
+                if (!GameManager.instance.rocks[0].activeSelf && !GameManager.instance.rocks[1].activeSelf && !GameManager.instance.rocks[2].activeSelf)
+                {
+                    Player.currentPhase = PlayerPhase.Adult;
+                    character.babyEnigma = true;
+                    character.UsePortal();
+                }
+            }
+            else
+            {
+                Debug.Log("Need to store each rock on the good recipient!");
+            }
+            // Change appareance here
+        }
+        // if (!waitActive)
+
+        //Debug.Log("Wait");
+        //StartCoroutine(Wait());
+
+        //player.transform.position = destinationPosition.position;
+
     }
-/*
-    IEnumerator Wait()
-    {
-        waitActive = true;
-        yield return new WaitForSeconds(2.0f);
-        waitActive = false;
-    }
-    */
+    /*
+        IEnumerator Wait()
+        {
+            waitActive = true;
+            yield return new WaitForSeconds(2.0f);
+            waitActive = false;
+        }
+        */
 
 }
 
